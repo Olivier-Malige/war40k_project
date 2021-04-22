eccbbfa94f73c10d62d68994d620711a0da6e6a7
maligeo <maligeo@kaliop.com>
2021-04-22 18:09:37 +0200

    add constants

---
+++ server_api/build/shared/constants.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.loginFailedErr = exports.paramMissingError = void 0;
+exports.paramMissingError = 'One or more of the required parameters was missing.';
+exports.loginFailedErr = 'Login failed';
