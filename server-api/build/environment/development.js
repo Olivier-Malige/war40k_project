8a457ba80e6375d51f6dba43ce8dd7850b748925
Olivier Malige <omalige@gmail.com>
2021-09-10 20:38:49 +0200

    fix docker

--- server-api/build/environment/development.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    dbUrl: 'mongodb+srv://arknoid:artgore666@cluster0.ujqdv.mongodb.net/warhammer_api?retryWrites=true&w=majority',
-    sessionSecret: '142ce4ad-3269-4592-9169-d39e87aabaa2',
-    port: 3001,
-};
