bc785c048d4f647e5823caf04860b6308e7f12fc
olivierm <olivier.malige@acelys.fr>
2021-10-06 09:34:07 +0200

    Fix background color

--- web-client/src/modules/navigation/Router.tsx
+++ web-client/src/modules/navigation/Router.tsx
@@ -8,27 +8,38 @@ import { UsersPage } from 'src/modules/navigation/pages/UsersPage';
 import UnitsEditor from 'src/modules/navigation/pages/UnitsEditor';
 import { ProtectedRoute } from './ProtectedRoute';
 import { Redirect } from 'react-router-dom';
+import { Box } from '@mui/system';
 
 const Router: React.FC = () => {
   return (
-    <Switch>
-      <ProtectedRoute component={HomePage} redirectTo={routeNames.LOGIN} path={routeNames.HOME} />
-      <ProtectedRoute
-        component={UnitsEditor}
-        redirectTo={routeNames.LOGIN}
-        path={routeNames.UNITS_EDITOR}
-      />
-      <ProtectedRoute component={UsersPage} redirectTo={routeNames.LOGIN} path={routeNames.USERS} />
-      <Route path={routeNames.LOGIN}>
-        <Authentication />
-      </Route>
-      <Route path="/">
-        <Redirect to={routeNames.HOME} />
-      </Route>
-      <Route path="*">
-        <NotFoundPage />
-      </Route>
-    </Switch>
+    <Box
+      sx={{
+        backgroundColor: theme => theme.palette.background.default,
+      }}
+    >
+      <Switch>
+        <ProtectedRoute component={HomePage} redirectTo={routeNames.LOGIN} path={routeNames.HOME} />
+        <ProtectedRoute
+          component={UnitsEditor}
+          redirectTo={routeNames.LOGIN}
+          path={routeNames.UNITS_EDITOR}
+        />
+        <ProtectedRoute
+          component={UsersPage}
+          redirectTo={routeNames.LOGIN}
+          path={routeNames.USERS}
+        />
+        <Route path={routeNames.LOGIN}>
+          <Authentication />
+        </Route>
+        <Route path="/">
+          <Redirect to={routeNames.HOME} />
+        </Route>
+        <Route path="*">
+          <NotFoundPage />
+        </Route>
+      </Switch>
+    </Box>
   );
 };
 
